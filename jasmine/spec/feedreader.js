0087249b5fd97f12201da7ec092104123ead0bc1
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-09-13 21:43:11 -0500

    test- loadFeed works

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -68,17 +68,32 @@ $(function() {
           * clicked and does it hide when clicked again.
           */
     });
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
+    
+    describe ('Initial Entries', function(){ /* TODO: Write a new test suite named "Initial Entries" */
+    
+    beforeEach(function(done){
+        loadFeed(0, done);
+    });
+        it ('loadFeed function called and completed', function(){
+            expect('title').not.toBe(0);
+            expect('contentSnippet').not.toBe(0);
+        })/* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
+     })
+    
+    describe('New Feed Selection', function(){/* TODO: Write a new test suite named "New Feed Selection" */
+    
+    beforeEach(function(done){
+        loadFeed(0, done);
+    });
+        it('new feed loaded', function(){
+            
+        })
+    })
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
